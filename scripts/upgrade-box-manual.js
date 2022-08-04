46114cc8441b7bdb33fb5f044213457df9054003
nikhil arora <narora200@gmail.com>
2022-08-05 01:38:08 +0530

    feat: upgrading contracts using hardhat & proxy

---
+++ scripts/upgrade-box-manual.js
@@ -0,0 +1,50 @@
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { network, deployments, deployer } = require("hardhat")
+
+async function main() {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+
+    const boxV2 = await deploy("BoxV2", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(boxV2.address, arguments)
+    }
+
+    // Upgrade!
+    // Not "the hardhat-deploy way"
+    const boxProxyAdmin = await ethers.getContract("BoxProxyAdmin")
+    const transparentProxy = await ethers.getContract("Box_Proxy")
+
+    const proxyBoxV1 = await ethers.getContractAt("Box", transparentProxy.address)
+    const versionV1 = await proxyBoxV1.version()
+    console.log("version before upgrade:", versionV1.toString())
+
+    const upgradeTx = await boxProxyAdmin.upgrade(transparentProxy.address, boxV2.address)
+    await upgradeTx.wait(1)
+
+    const proxyBoxV2 = await ethers.getContractAt("BoxV2", transparentProxy.address)
+    const version2 = await proxyBoxV2.version()
+    console.log("version after upgrade:", version2.toString())
+    log("----------------------------------------------------")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
